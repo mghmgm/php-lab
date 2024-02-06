4d7a7d28bc2aeb330984b9ac7063848e3392f1dd
irina <irina.yaltantseva@mail.ru>
2024-02-06 17:29:35 +0300

    hello,world

---
+++ lab-1/readme.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        header {
+            display: flex;
+        }  
+        main {
+            display: flex;
+            justify-content: center;
+        }
+        h1 {
+            margin-left: 170px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <img src="logo.jpg" alt="logo" width='250px' height='70px'>
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+    <main>
+        <h2><?php echo 'Hello, PHP';?></h2>
+    </main>
+    <footer>
+        <ul>
+            <li>footer item</li>
+            <li>footer item</li>
+            <li>footer item</li>
+        </ul>
+    </footer>
+</body>
+</html>
