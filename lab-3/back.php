d502adc17112a7d2832b3e00f2a8869b3485e599
irina <irina.yaltantseva@mail.ru>
2024-02-13 17:01:48 +0300

    lab3

--- lab-3/back.php
+++ lab-3/back.php
@@ -4,14 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        main {
+            margin: 50px;
+        }
+    </style>
 </head>
 <body>
-    <?php 
-        $headers = get_headers('http://127.0.0.1/php-курс/lab-3/index.html');
-        foreach ($headers as $element){
-            echo $element . "<br>";
-        }
-    ?>
-    <a href="index.html">Переход на 1 страницу</a>
+    <head> 
+        <img src="logo.jpg" width='220px' height='60px' alt="">
+    </head>
+    <main>
+        <?php 
+            $headers = get_headers('http://127.0.0.1/php-курс/lab-3/index.html');
+            foreach ($headers as $element){
+                echo $element . "<br>";
+            }
+        ?>
+        <a href="index.html">Переход на 1 страницу</a>
+    </main>
+    <footer>
+        <ul>
+            <li>footer item</li>
+            <li>footer item</li>
+            <li>footer item</li>
+        </ul>
+    </footer>
 </body>
 </html>
