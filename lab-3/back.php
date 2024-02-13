90fd69b9df0fed9b5ca07da7e42988b586be8baf
irina <irina.yaltantseva@mail.ru>
2024-02-13 16:06:29 +0300

    lab3

---
+++ lab-3/back.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        print_r(get_headers('https://www.php.net/manual/ru/book.sqlite3.php', true));
+    ?>
+    <a href="index.html">Переход на 1 страницу</a>
+</body>
+</html>
